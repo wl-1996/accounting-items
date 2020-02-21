8f8ca8ac4a24cc2621a5006db745332d601152af
WK <741341649@qq.com>
2020-02-21 15:22:43 +0800

    创建<Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg class="icon">
+        <use :xlink:href="'#' + name"/>
+    </svg>
+</template>
+
+<script lang="ts">
+    const importAll = (requireContent: __WebpackModuleApi.RequireContext) => requireContent.keys().forEach(requireContent);
+    try {
+        importAll(require.context('../assets/icons', true, /\.svg$/));
+    } catch (error) {
+        console.log(error);
+    }
+
+    export default {
+        name: 'Icon',
+        props:['name']
+    };
+</script>
+
+<style lang="scss" scoped>
+    .icon{
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,30 @@
 <template>
     <div class="nav">
-        <router-link to="/money">记账页</router-link>
+        <router-link to="/money">
+            <Icon name="money"/>
+            记账页
+        </router-link>
         |
-        <router-link to="/labels">标签页</router-link>
+        <router-link to="/labels">
+            <Icon name="label"/>
+            标签页
+        </router-link>
         |
-        <router-link to="/statistics">统计页</router-link>
+        <router-link to="/statistics">
+            <Icon name="statistics"/>
+            统计页
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
     export default {
-        name: 'Nav'
+        name: 'Nav',
     };
 </script>
 
 <style lang="scss" scoped>
-    .nav{
+    .nav {
         border: 1px solid blue;
     }
 </style>

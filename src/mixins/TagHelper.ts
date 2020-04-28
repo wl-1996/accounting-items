41f0029475970d9acdcb7ae902ef9ad521f24ff2
WK <741341649@qq.com>
2020-04-28 18:06:35 +0800

    判断输入是否是空格逻辑修改

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -5,14 +5,18 @@ import Component from 'vue-class-component';
 export class TagHelper extends Vue {
     createTag() {
         const name = window.prompt('请输入标签名');
-        if (!name || name.match(/^[ ]*$/)) {
-            return window.alert('标签名不能为空');
-        } else {
-            this.$store.commit('createTag', name);
-            if (this.$store.state.createTagError) {
-                window.alert(this.$store.state.createTagError.message);
-            }else{
-                window.alert('添加成功')
+        if (name) {
+            const name2 = name.trim();
+            console.log(name2);
+            if (name2.length === 0) {
+                window.alert('标签名不能为空');
+            } else {
+                this.$store.commit('createTag', name);
+                if (this.$store.state.createTagError) {
+                    window.alert(this.$store.state.createTagError.message);
+                } else {
+                    window.alert('添加成功');
+                }
             }
         }
     }
